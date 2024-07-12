33ce9b3b4ac08d51a0cf896bf481c94ee54af3f2
Adeel Mujahid <3840695+am11@users.noreply.github.com>
2024-07-12 17:13:07 +0300

    Ensure correct VC++ toolset selection for AOT

--- src/coreclr/nativeaot/BuildIntegration/findvcvarsall.bat
+++ src/coreclr/nativeaot/BuildIntegration/findvcvarsall.bat
@@ -10,7 +10,20 @@ SET vswherePath=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.ex
 IF NOT EXIST "%vswherePath%" GOTO :ERROR
 
 SET toolsSuffix=x86.x64
-IF /I "%~1"=="arm64" SET toolsSuffix=ARM64
+IF /I "%PROCESSOR_ARCHITECTURE%" == "arm64" (
+    SET vcEnvironment=arm64
+    IF /I "%~1" == "x64" ( SET vcEnvironment=arm64_amd64 )
+    IF /I "%~1" == "x86" ( SET vcEnvironment=arm64_x86 )
+    IF /I "%~1" == "arm64" ( SET toolsSuffix=arm64 )
+) ELSE IF /I "%PROCESSOR_ARCHITECTURE%" == "amd64" (
+    SET vcEnvironment=amd64
+    IF /I "%~1" == "x86" ( SET vcEnvironment=amd64_x86 )
+    IF /I "%~1" == "arm64" ( SET vcEnvironment=amd64_arm64 )
+) ELSE (
+    SET vcEnvironment=x86
+    IF /I "%~1" == "x64" ( SET vcEnvironment=amd64_x86 )
+    IF /I "%~1" == "arm64" ( SET vcEnvironment=x86_arm64 )
+)
 
 FOR /F "tokens=*" %%i IN (
     '"%vswherePath%" -latest -prerelease -products * ^
@@ -20,22 +33,6 @@ FOR /F "tokens=*" %%i IN (
 ) DO SET vsBase=%%i
 
 IF "%vsBase%"=="" GOTO :ERROR
-
-SET procArch=%PROCESSOR_ARCHITEW6432%
-IF "%procArch%"=="" SET procArch=%PROCESSOR_ARCHITECTURE%
-
-SET vcEnvironment=%~1
-IF /I "%~1"=="x64" (
-    SET vcEnvironment=x86_amd64
-    IF /I "%procArch%"=="AMD64" SET vcEnvironment=amd64
-)
-IF /I "%~1"=="arm64" (
-    SET vcEnvironment=x86_arm64
-    IF /I "%procArch%"=="AMD64" SET vcEnvironment=amd64_arm64
-)
-IF /I "%~1"=="x86" (
-    IF /I "%procArch%"=="AMD64" SET vcEnvironment=amd64_x86
-)
 
 CALL "%vsBase%\vc\Auxiliary\Build\vcvarsall.bat" %vcEnvironment% > NUL
 

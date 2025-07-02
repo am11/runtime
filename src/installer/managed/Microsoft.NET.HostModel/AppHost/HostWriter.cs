8b2d4ce3a09744d9b5f38feb6ed39884a0134e82
Adeel <3840695+am11@users.noreply.github.com>
2025-07-02 04:13:22 +0300

    Use SetUnixFileMode

--- src/installer/managed/Microsoft.NET.HostModel/AppHost/HostWriter.cs
+++ src/installer/managed/Microsoft.NET.HostModel/AppHost/HostWriter.cs
@@ -179,7 +179,13 @@ namespace Microsoft.NET.HostModel.AppHost
                         }
                     }
                 });
-                Chmod755(appHostDestinationFilePath);
+
+                if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    // chmod +755
+                    File.SetUnixFileMode(appHostDestinationFilePath, UnixFileMode.UserRead | UnixFileMode.UserWrite | UnixFileMode.UserExecute |
+                        UnixFileMode.GroupRead | UnixFileMode.GroupExecute | UnixFileMode.OtherRead | UnixFileMode.OtherExecute);
+                }
             }
             catch (Exception ex)
             {
@@ -219,28 +225,58 @@ namespace Microsoft.NET.HostModel.AppHost
 
             return searchOptionsBytes;
         }
-
-        internal static void Chmod755(string pathName)
-        {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                return;
-            var filePermissionOctal = Convert.ToInt32("755", 8); // -rwxr-xr-x
-            const int EINTR = 4;
-            int chmodReturnCode;
-
-            do
-            {
-                chmodReturnCode = chmod(pathName, filePermissionOctal);
-            }
-            while (chmodReturnCode == -1 && Marshal.GetLastWin32Error() == EINTR);
-
-            if (chmodReturnCode == -1)
-            {
-                throw new Win32Exception(Marshal.GetLastWin32Error(), $"Could not set file permission {Convert.ToString(filePermissionOctal, 8)} for {pathName}.");
-            }
-        }
-
-        [LibraryImport("libc", SetLastError = true)]
-        private static partial int chmod([MarshalAs(UnmanagedType.LPStr)] string pathname, int mode);
     }
 }
+
+#if NETFRAMEWORK
+[Flags]
+internal enum UnixFileMode
+{
+    None = 0,
+    OtherExecute = 1,
+    OtherWrite = 2,
+    OtherRead = 4,
+    GroupExecute = 8,
+    GroupWrite = 16,
+    GroupRead = 32,
+    UserExecute = 64,
+    UserWrite = 128,
+    UserRead = 256,
+    StickyBit = 512,
+    SetGroup = 1024,
+    SetUser = 2048,
+}
+
+internal static class FileExtensions
+{
+    extension(File)
+    {
+        public static void SetUnixFileMode(string path, UnixFileMode mode)
+        {
+            int user = ((mode & UnixFileMode.UserRead)   != 0 ? 4 : 0)
+                     | ((mode & UnixFileMode.UserWrite)  != 0 ? 2 : 0)
+                     | ((mode & UnixFileMode.UserExecute)!= 0 ? 1 : 0);
+            int group = ((mode & UnixFileMode.GroupRead)   != 0 ? 4 : 0)
+                      | ((mode & UnixFileMode.GroupWrite)  != 0 ? 2 : 0)
+                      | ((mode & UnixFileMode.GroupExecute)!= 0 ? 1 : 0);
+            int other = ((mode & UnixFileMode.OtherRead)   != 0 ? 4 : 0)
+                      | ((mode & UnixFileMode.OtherWrite)  != 0 ? 2 : 0)
+                      | ((mode & UnixFileMode.OtherExecute)!= 0 ? 1 : 0);
+            int octal = (user << 6) | (group << 3) | other;
+
+            const int EINTR = 4;
+            int res;
+            do
+            {
+                res = chmod(path, octal);
+            } while (res == -1 && Marshal.GetLastWin32Error() == EINTR);
+
+            if (res == -1)
+                throw new Win32Exception(Marshal.GetLastWin32Error(), $"Could not set file permission {Convert.ToString(octal, 8)} for {path}.");
+
+            [DllImport("libc", SetLastError = true)]
+             static extern int chmod([MarshalAs(UnmanagedType.LPStr)] string pathname, int mode);
+        }
+    }
+}
+#endif
